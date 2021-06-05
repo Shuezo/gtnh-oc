042be054ec6ec9f43612aef8211e915b14156ad6
Sean Huezo <seanahuezo@hotmail.com>
2021-06-05 18:55:10 -0400

    add functions

--- PowerMonitor.lua
+++ PowerMonitor.lua
@@ -7,8 +7,10 @@ local gpu = component.gpu--can
 local w, h = gpu.getResolution()
 
 --Set Component ID's
-local reactor = component.de831599-fabb-44a5-ac3c-4ac71a2f16f5
-local chest = component.fa458337-2bdd-4161-94f1-c126ce8571ef
-local bat = component.e4ecc183-dfe1-4fd0-a68f-56589d54902b
+--local reactor = component.de831599-fabb-44a5-ac3c-4ac71a2f16f5
+--local chest = component.fa458337-2bdd-4161-94f1-c126ce8571ef
+--local bat = component.e4ecc183-dfe1-4fd0-a68f-56589d54902b
 
-gpu.fill(1, 1, w, h, " ") --clears the screen
+function clear () gpu.fill(1, 1, w, h, " ") end --clears the screen
+
+function Fill (x) return x end
