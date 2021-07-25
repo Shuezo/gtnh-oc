96fee9e62a2ee5d63fd07facfda41bb3debddbeb
Sean Huezo <seanahuezo@hotmail.com>
2021-07-25 11:19:20 -0400

    Pulled from Page-Update

--- Util/Update.lua
+++ Util/Update.lua
@@ -30,6 +30,7 @@ local function getHTTPData(url) --gets HTTP data and returns it in a table
     local req, resp = pcall(internet.request, url)
     
     if req then
+        while resp.response() == nil do os.sleep(0.1) end
         local code, _, _ = resp.response()
 
         if code == 200 then
