0220e80a5c8ae7b2e5f53261bf8e4d8907de6f8a
derbicus <derbycar379@gmail.com>
2021-09-28 22:44:01 -0400

    debug

--- Components/GtMachine.lua
+++ Components/GtMachine.lua
@@ -41,8 +41,8 @@ end
 
 function GtMachine:hasProblems()
     if string.sub( self.getSensorInformation()[5], 13, 14 ) == 'c0' then return false                       --Cleanroom, EBF
-    elseif string.sub( self.getSensorInformation()[7], 13, 14 ) == 'c0' then return false                       --Pyrolyse Oven
     elseif self.getSensorInformation()[2] == '§aNo Maintenance issues§r' then return false                  --Turbine
+    elseif string.sub( self.getSensorInformation()[7], 13, 14 ) == 'c0' then return false                       --Pyrolyse Oven
     elseif self.getSensorInformation()[9] == 'Maintenance Status: §aWorking perfectly§r' then return false  --LSC
     elseif self.getSensorInformation()[31] == 'Maintenance Status: §aWorking perfectly§r' then return false --TFFT
     else return true
